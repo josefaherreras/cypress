ba0534183e6ba93d6aaf0bf4f4f1dfd3e89862d5
josefaherreras <josefaigna.herreras@gmail.com>
2021-01-21 11:14:12 -0300

    caso de prueba con search

---
+++ cypress/integration/search.js
@@ -0,0 +1,8 @@
+describe('Search test case', function(){
+    it('Search with results', function(){
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('#search_query_top').type('dress');
+        cy.get('#searchbox > .btn').click();
+        cy.get('.lighter').contains('"dress"');
+    })
+})
