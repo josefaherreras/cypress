b212ebd85a25ba72313cd16ad725e3a872aefb63
josefaherreras <josefaigna.herreras@gmail.com>
2021-01-22 10:37:54 -0300

    Cypress Before & After

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,8 +1,20 @@
 describe('Search test case', function(){
-    it('Search with results', function(){
+    before(function(){
+        cy.log('ejecutando precondiciones a las pruebas')
+    })
+    after(function(){
+        cy.log('ejecutando postcondiciones a las pruebas')
+    })
+    it('Search dresses', function(){
         cy.visit('http://automationpractice.com/index.php');
         cy.get('#search_query_top').type('dress');
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains('"dress"');
+    })
+    it('Search hats', function(){
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('#search_query_top').type('hat');
+        cy.get('#searchbox > .btn').click();
+        cy.get('.lighter').contains('"hat"');
     })
 })
