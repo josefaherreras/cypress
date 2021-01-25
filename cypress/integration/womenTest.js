714b177029be83fc4daf1bcba4c244eb9f6ebb29
josefaherreras <josefaigna.herreras@gmail.com>
2021-01-25 16:57:30 -0300

    Cypress manejo de Checkboxes

---
+++ cypress/integration/womenTest.js
@@ -0,0 +1,13 @@
+describe('Pruebas en el sitio women',function(){
+    beforeEach(function(){
+        cy.visit('http://automationpractice.com/index.php?id_category=3&controller=category');
+    })
+    it('Large tops search', function(){
+        cy.get('#layered_category_4').check();
+        cy.get('#layered_id_attribute_group_3').check();
+        /* probando mas cosas
+        y mas cosas 
+        y mas cosas */
+        cy.get('#layered_id_attribute_group_3').uncheck();
+    })
+})
