ebbfa291092724b2fc0fff7096086d59ab6ce8a6
josefaherreras <josefaigna.herreras@gmail.com>
2021-01-28 13:16:00 -0300

    Cypress  Seleccionando cualquier elemento de la lista con eq()

--- cypress/integration/goToSections.js
+++ cypress/integration/goToSections.js
@@ -1,7 +1,13 @@
 /// <reference types="cypress"/>
 describe('Go to different sections ', function(){
-    it('go to women section', function(){
+    beforeEach(function(){
         cy.visit('http://automationpractice.com/index.php');
+    })
+    it('go to women section', function(){
         cy.get('[class=sf-with-ul]').first().click();
+    })
+    it("Go to Dresses section", function(){
+        //cual elemento quieres tomar 
+        cy.get('[class=sf-with-ul]').eq(3).click();
     })
 })
